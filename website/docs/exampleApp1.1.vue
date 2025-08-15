35d432aa910d77aadde202d54982692fbe006737
Sai Pavan Kumar <gandhipavan555@gmail.com>
2025-08-15 23:40:40 +0530

    Create exampleApp1.1.vue
    Options API + composition API combined with reactive state usage

---
+++ website/docs/exampleApp1.1.vue
@@ -0,0 +1,21 @@
+<script>
+import {ref} from 'vue'
+export default {
+  setup() {
+
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+    }
+    return {
+      count,
+      increment
+    }
+  }
+  
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
