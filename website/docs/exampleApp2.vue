ec0006ada73f42e2cfb00abaa42de783dfe31132
Sai Pavan Kumar <gandhipavan555@gmail.com>
2025-08-15 23:05:24 +0530

    Create exampleApp2.vue
    Composition API
    
    Example for Counter And OnClick's

---
+++ website/docs/exampleApp2.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+// component logic
+// declare some reactive state here.
+  const message = ref("Hey Make me dynamic")
+  
+  const onMessageClick = () =>{
+    console.log("clicked")
+    message.value="Dynamically Changed"
+  }
+  const count = reactive({
+    counter: 0
+  })
+  const increment = () => {
+    count.counter++;
+  }
+  const decrement = () => {
+    count.counter--;
+  }
+  
+  const reset = () => {
+    count.counter=0;
+  }
+  
+</script>
+
+<template>
+  <h1 v-on:click="onMessageClick">{{message.split("").reverse().join("")}}</h1>
+  <p>
+    Counter is : {{count.counter}}
+  </p>
+  <button v-on:click="increment">
+    Increment
+  </button>
+  <button v-on:click="reset">
+    Reset
+  </button>
+  <button v-on:click="decrement">
+    Decrement
+  </button>
+</template>
