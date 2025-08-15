76c533ef7bc80455a0883f72ff5450ea7827347c
Sai Pavan Kumar <gandhipavan555@gmail.com>
2025-08-16 00:03:31 +0530

    Create Attribute-Bindings.vue

---
+++ website/Attribute-Bindings.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me red</h1> <!-- add dynamic class binding here -->
+</template>
+
+<style>
+.title {
+  color: red;
+  background-color: aqua;
+  box-shadow:2px 4px;
+  padding: 20px;
+  font-family: arial;
+}
+</style>
